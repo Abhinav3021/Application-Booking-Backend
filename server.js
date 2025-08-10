02cceb1782ece29c2732f5ec48524b05ef781554
Abhinav3021 <124438690+Abhinav3021@users.noreply.github.com>
2025-08-10 20:34:24 +0530

    Commit

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Use an environment variable for the frontend URL
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Connect to MongoDB
